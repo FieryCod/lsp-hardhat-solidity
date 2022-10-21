f4d03106e0a95d392bcd337fdc21d763855b202c
Karol Wójcik <karol.wojcik@tuta.io>
2022-10-22 01:30:54 +0200

    Initial commit

---
+++ lsp-hardhat-solidity.el
@@ -0,0 +1,33 @@
+;;; lsp-hardhat-solidity.el --- Emacs lsp-mode support for hardhat-solidity -*- lexical-binding: t; -*-
+
+;; Version: 0.1
+
+;; Package-Requires: ((emacs "25.1") (lsp-mode "6.0") (markdown-mode "2.3") (dash "2.18.0") (f "0.20.0") (ht "2.0") (request "0.3.0") (treemacs "2.5") (dap-mode "0.5"))
+;; Keywords: languague, tools
+;; URL: https://github.com/emacs-lsp/lsp-java
+;; Author: Karol Wójcik <karol.wojcik@tuta.io>
+;; Maintainer: Karol Wójcik <karol.wojcik@tuta.io>
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(provide 'lsp-hardhat-solidity)
+;;; lsp-hardhat-solidity.el ends here
+
+;; Local Variables:
+;; flycheck-disabled-checkers: (emacs-lisp-checkdoc)
+;; End:
